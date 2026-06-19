54f342a373d86f66c83a181e3e55ef850286fad3
Vy Anh <vya123.work@gmail.com>
2026-06-19 22:58:17 +0700

    fix import path

--- src/modelling.py
+++ src/modelling.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
-from config import * 
+from src.config import * 
 
 warnings.filterwarnings("ignore")
 
